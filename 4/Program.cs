38342e01258c8464d192ee20b549b22d0d70b6a5
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-09-19 22:12:49 +0300

    Второе, которе четвертое

---
+++ 4/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int[] nums = new int[3] { a, b, c};
+for (int i = 1; i < 3; i++){
+   
+    if (max < nums[i])
+        max = nums[i];
+}
+Console.WriteLine(max);
