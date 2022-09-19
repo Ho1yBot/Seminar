14312de7659d00e14bd317fee05f20d09bf0ee16
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-09-19 22:53:17 +0300

    третье, которе шестое

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,11 +1,12 @@
 ﻿int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
 int[] nums = new int[3] { a, b, c};
+Console.WriteLine(nums[0]);
 for (int i = 0; i < 3; i++){
    
-    if (max < nums[i])
-        max = nums[i];
+    if (nums[i] % 2 == 0 )
+        Console.WriteLine(nums[i] + " - четное");
+    else
+        Console.WriteLine(nums[i] + " - нечетное");
 }
-Console.WriteLine(max);
