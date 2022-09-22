73f76ce63887423bf0c46e4a63ae1bffe55c4919
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-09-23 00:36:53 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,3 @@
+﻿string n = Convert.ToString(new Random().Next(100,1000));
+Console.WriteLine("Исходное число: " + n);
+Console.WriteLine("Вторая цифра числа: " + n[1]);
--- Доп/Program.cs
+++ Доп/Program.cs
@@ -1,11 +1,20 @@
-﻿// while ((int n = Convert.ToInt32(Console.ReadLine())) != 0){
-//     double max = 
-// //Console.WriteLine(terms[]);
-
-/*
-Я не знаю как это написать на C#. Мы в цикле создаём новую переменную и сравниваем её с нулём. 
-далее в цикле каждое новое число записываем в массив и как только задаётся 0 - программа выходит из цикла.
-Мы имеем массив с числами. Прогоняем его в цикле for, записывая максимальное и второе по максимальности
- сравнивая их между собой и с каждым в массиве. 
- Выводим второе по максимальности. 
- Самое главное мне понять как написать программу до этапа с for, помогите)
+﻿int max1 = 0;
+int max2 = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+do {
+    n = Convert.ToInt32(Console.ReadLine());
+    if (max1 > n){
+        max2 = max1;
+        max1 = n;
+    }
+    else if (max2 > n){
+        max2 = n;
+    }
+    
+}
+while (n != 0);
+Console.WriteLine(max1);
+if (max1 > max2)
+    Console.WriteLine(max1);
+else
+    Console.WriteLine(max2);
