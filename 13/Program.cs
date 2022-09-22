3a4d2a3f01af8522de4c1b1467b68069bc8bcb08
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-09-23 00:58:40 +0300

    13

---
+++ 13/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+*/
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99){
+    string t = n.ToString();
+    Console.WriteLine(t[2]);
+}
+else{
+    //string t = n.ToString();
+    Console.WriteLine(n + " --> третьей цифры нет");
+}
