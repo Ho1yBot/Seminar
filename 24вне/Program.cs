b5a581b2cd4d2ed3616ff60ccbe204df35228cb6
Svyatoslav <pozolotin-cvat@yandex.ru>
2022-09-28 21:51:01 +0300

    24вне

---
+++ 24вне/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 --> 28
+4 --> 10
+8 --> 36
+*/
+int res = 0;
+int a = Convert.ToInt32(Console.ReadLine());
+int deio(int a)
+{
+    //int a = Convert.ToInt32(Console.ReadLine());
+    int i = 1;
+    for(i=1; i<=a; i++) 
+    {
+        res = res + i;
+        
+    }
+    return res;
+}
+int result = deio(a); 
+Console.Write(result);
